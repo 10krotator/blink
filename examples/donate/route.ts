fdb1e3a8447afbd3077eed8442a0e5cc001532aa
rcsang <sangmath@outlook.com>
2024-06-25 16:09:06 +0200

    commit

--- examples/donate/route.ts
+++ examples/donate/route.ts
@@ -142,7 +142,7 @@ function getDonateInfo(): Pick<
   'icon' | 'title' | 'description'
 > {
   const icon =
-    'https://ucarecdn.com/7aa46c85-08a4-4bc7-9376-88ec48bb1f43/-/preview/880x864/-/quality/smart/-/format/auto/';
+    'https://ucarecdn.com/6e28d269-cc42-49f6-a586-41405fd56763/-/preview/1000x333/';
   const title = 'Solana to $10K';
   const description =
     'Engineer | Soylana manlet.';
